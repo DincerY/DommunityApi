667985d0f8d433009cfe77dff813a80f81be211c
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-28 13:23:56 +0300

    Add project files.

---
+++ Infrastructure/Dommunity.Persistence/Contexts/DommunityDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Dommunity.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Dommunity.Persistence.Contexts;
+
+public class DommunityDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+}
